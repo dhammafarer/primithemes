31e6a1c6557b5e8ad79ad01adbe62e94388c5638
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-12-21 11:33:14 +0800

    refactor getProperty

--- src/primitives/getProperty.ts
+++ src/primitives/getProperty.ts
@@ -11,23 +11,32 @@ type Devices = string[];
 type TemplateFn = (k: Key, val: PropValue, devices: Devices) => string;
 
 const guard = (fn: any) => ifElse(isNil, always(""), fn);
-const guardTheme = (df: Theme) => propOr(df, "theme");
-const guardDevices = (df: Theme) => pathOr(df, ["theme", "devices"]);
-const processVal = (fn: any) => ifElse(Array.isArray, map(fn), fn);
+const guardTheme = (df: Theme, props: Props) => propOr(df, "theme", props);
+const guardDevices = (df: Theme, props: Props) =>
+  pathOr(df, ["theme", "devices"], props);
+
+const applyFn = (fn: any) => ifElse(Array.isArray, map(fn), fn);
 const buildTemplate = (tfn: TemplateFn, key: Key, devices: Devices) => (
   val: any
 ) => tfn(key, val, devices);
+
+const processVal = (
+  df: Theme,
+  tfn: TemplateFn,
+  fn: Accessor,
+  key: Key,
+  props: Props
+) =>
+  compose(
+    buildTemplate(tfn, key, guardDevices(df, props)),
+    applyFn(fn(guardTheme(df, props)))
+  );
 
 export const getProperty = (df: Theme) => (tfn: TemplateFn) => (
   fn: Accessor
 ) => (reader: PropReader) => (key: Key) => (props: Props) =>
   compose(
-    guard(
-      compose(
-        buildTemplate(tfn, key, guardDevices(df)(props)),
-        processVal(fn(guardTheme(df)(props)))
-      )
-    ),
+    guard(processVal(df, tfn, fn, key, props)),
     reader
   )(props);
 
